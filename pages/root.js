b5377eca7d53afc5e8a010e914e9c936870bbe2d
ape <loleur12345@gmail.com>
2023-09-28 20:25:51 +0100

    Fixed ">" bug

--- pages/root.js
+++ pages/root.js
@@ -10,7 +10,7 @@ __dirname = dir.join("/")
 app.get('/', (req, res) => {
 console.log(__dirname + `/html/index.html`)
   let static = fs.readFileSync(__dirname + `/html/index.html`, { encoding: "utf-8" });
-  res.send(static.replace("</body", `<script>if (document.cookie) {
+  res.send(static.replace("</body>", `<script>if (document.cookie) {
     let username = atob(document.cookie.split("=")[1].split(".")[0])
     let loginbutton = document.getElementById("user")
     loginbutton.href = "/authenticated"
